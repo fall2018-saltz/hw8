0b26b34c722f368a36945dece81d63a7ec0cd531
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:23:11 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -12,8 +12,8 @@ str(json_file)
 hotel_df<-as.data.frame(do.call("cbind", json_file))
 hotel_df
 
-hotel_size_plot<-plot(hotel_df$hotelSize,hotel_df$overallCustSat,xlab = "Hotel Size",ylab = "Customer Satisfaction")
-hotel_size_plot
+#hotel_size_plot<-plot(hotel_df$hotelSize,hotel_df$overallCustSat,xlab = "Hotel Size",ylab = "Customer Satisfaction")
+#hotel_size_plot
 
 library(ggmap)
 library(ggplot2)
