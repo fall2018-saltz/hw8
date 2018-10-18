1a09c444535d02541fc6fbf7c1c6ed82cfc6f4e2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 13:35:23 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -10,3 +10,5 @@ str(json_file)
 
 hotel_df<-as.data.frame(do.call("cbind", json_file))
 hotel_df
+
+hotel_size_plot<-plot(hotel_df$hotelSize,hotel_df$overallCustSat,xlab = "Hotel Size",ylab = "Customer Satisfaction")
