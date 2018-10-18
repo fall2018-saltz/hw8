955df62b1b618e7bc165460b7badc7965ccf4c6f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 11:15:30 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -6,3 +6,4 @@
 
 library(rjson)
 json_data<-fromJSON(getURL('https://blackboard.syracuse.edu/bbcswebdav/pid-5159146-dt-content-rid-38224657_1/courses/21341.1191/hotelSurveySherison.json'))
+json_data
