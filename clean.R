868b62a18187dff90f75ddc1d66de2c14a8f3650
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-18 14:21:56 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -14,3 +14,13 @@ hotel_df
 
 hotel_size_plot<-plot(hotel_df$hotelSize,hotel_df$overallCustSat,xlab = "Hotel Size",ylab = "Customer Satisfaction")
 hotel_size_plot
+
+library(ggmap)
+library(ggplot2)
+
+hotel_df$hotelState<-tolower(hotel_df$hotelState)
+
+
+state_map<-ggplot(hotel_df,aes(map_id=hotelState))+geom_map(map=US,aes(fill=overallCustSat),color="black")
+state_map<-state_map+expand_limits(x=US$long,y=US$lat)+coord_map()+ggtitle("Customer Satisfaction according to state")
+state_map
